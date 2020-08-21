851f9fc3e32cac32c2b163e5f6f5ef1f07567c62
fernando <fdtroya@espol.edu.ec>
2020-08-21 01:33:27 -0500

    calculo de aceleraciones de centros de gravedad

--- kinematics.py
+++ kinematics.py
@@ -30,6 +30,16 @@ theta1_dot=0
 theta1_doubledot=0
 theta1_doubledots=[]
 
+acg2x=[]
+acg2y=[]
+
+acg1x=[]
+acg1y=[]
+
+acg3x=[]
+acg3y=[]
+
+
 
 def displacement(p):
     theta3, theta1 = p
@@ -59,17 +69,57 @@ for i in range(resolution):
     theta2s.append(theta2)
     theta3_doubledots.append(theta3_doubledot)
     theta1_doubledots.append(theta1_doubledot)
+    acg2x_o=-(r2/2)*(math.cos(theta2)*theta2_dot**2)
+    acg2y_o=-(r2/2)*(math.sin(theta2)*theta2_dot**2)
+    acg1x_o=2*acg2x_o-(r1/2)*(math.cos(theta1)*theta1_dot**2+theta1_doubledot*math.sin(theta1))
+    acg1y_o=2*acg2y_o+(r1/2)*(-math.sin(theta1)*theta1_dot**2+theta1_doubledot*math.cos(theta1))
+    acg3x_o=(-r3/2)*(math.cos(theta3)*theta3_dot**2+theta3_doubledot*math.sin(theta3))
+    acg3y_o=(r3/2)*(-math.sin(theta3)*theta3_dot**2+theta3_doubledot*math.cos(theta3))
+
+    acg2x.append(acg2x_o)
+    acg2y.append(acg2y_o)
+    acg1x.append(acg1x_o)
+    acg1y.append(acg1y_o)
+    acg3x.append(acg3x_o)
+    acg3y.append(acg3y_o)
+    
+
+
+
     
 
 plt.xlabel("theta 2 [rad]")
 plt.plot(theta2s,theta1_doubledots)
-plt.ylabel("alpha 1 [rad/s**2]")
+plt.ylabel("alpha 1 [rad/s^2]")
 plt.show()
 plt.plot(theta2s,theta3_doubledots)
-plt.ylabel("alpha 3 [rad/s**2]")
+plt.ylabel("alpha 3 [rad/s^2]")
+plt.xlabel("theta 2 [rad]")
 plt.show()
-
-
+plt.plot(theta2s,acg2x)
+plt.ylabel("acg2x [m/s^2]")
+plt.xlabel("theta 2 [rad]")
+plt.show()
+plt.plot(theta2s,acg2y)
+plt.ylabel("acg2y [m/s^2]")
+plt.xlabel("theta 2 [rad]")
+plt.show()
+plt.plot(theta2s,acg1x)
+plt.ylabel("acg1x [m/s^2]")
+plt.xlabel("theta 2 [rad]")
+plt.show()
+plt.plot(theta2s,acg1y)
+plt.ylabel("acg1y [m/s^2]")
+plt.xlabel("theta 2 [rad]")
+plt.show()
+plt.plot(theta2s,acg3x)
+plt.ylabel("acg3x [m/s^2]")
+plt.xlabel("theta 2 [rad]")
+plt.show()
+plt.plot(theta2s,acg3y)
+plt.ylabel("acg3y [m/s^2]")
+plt.xlabel("theta 2 [rad]")
+plt.show()
 
 
 
